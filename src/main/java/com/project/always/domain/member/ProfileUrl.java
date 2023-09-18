8c39b73fd4c999cbf842bc13bfe8d818468d7864
COBI-98 <tkdgus968@naver.com>
2023-09-19 03:31:10 +0900

    feat: 초기 엔티티 생성

---
+++ src/main/java/com/project/always/domain/member/ProfileUrl.java
@@ -0,0 +1,12 @@
+package com.project.always.domain.member;
+
+import javax.persistence.Embeddable;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Embeddable
+@NoArgsConstructor
+public class ProfileUrl {
+
+}
