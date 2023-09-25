35abf38a85ce7cf960c33662cfb85bdbcb917e7b
doomdabo <bbo1209@naver.com>
2023-09-25 11:41:23 +0900

    feat: 네이버 로그인 기능 구현
    토큰 받아와서 포스트맨으로 실험 완료. 추후 테스트 코드 작성해서 DB랑 연결되게 해야함

---
+++ src/main/java/com/project/always/security/oauth/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.project.always.security.oauth.repository;
+
+import com.project.always.security.oauth.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
