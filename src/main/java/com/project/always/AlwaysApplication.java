cc585f38b490c74dc6e609ab2ae28bd472034287
COBI-98 <tkdgus968@naver.com>
2023-11-11 23:40:08 +0900

    feat: Elasticsearch Clients 설정

--- src/main/java/com/project/always/AlwaysApplication.java
+++ src/main/java/com/project/always/AlwaysApplication.java
@@ -1,8 +1,15 @@
 package com.project.always;
 
+import com.project.always.utils.elasticsearch.repository.BarSearchRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(
+		type = FilterType.ASSIGNABLE_TYPE,
+		classes = BarSearchRepository.class))
 @SpringBootApplication
 public class AlwaysApplication {
 
