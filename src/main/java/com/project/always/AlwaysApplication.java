dff2d2928347016a61e78800760fd9f45d6109f2
COBI-98 <tkdgus968@naver.com>
2023-11-11 23:42:10 +0900

    style: 패키지 구조 수정

--- src/main/java/com/project/always/AlwaysApplication.java
+++ src/main/java/com/project/always/AlwaysApplication.java
@@ -1,6 +1,6 @@
 package com.project.always;
 
-import com.project.always.utils.elasticsearch.repository.BarSearchRepository;
+import com.project.always.bar.elasticsearch.repository.BarSearchRepository;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
